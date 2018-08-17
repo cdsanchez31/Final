93e73a3053ee1c99253a40cf717f9d5bbc3c1bc8
Carlos Sanchez <cdsancheez31@miena.edu.co>
2018-08-16 23:38:22 -0500

    Trabajo en Casa

--- pages/profile.php
+++ pages/profile.php
@@ -77,7 +77,7 @@ if(!isset($_SESSION['usuario']))
 
               <h3 class="profile-username text-center"><?php echo $_SESSION['nombre']?> <?php echo $_SESSION['apellido']?></h3>
 
-              <p class="text-muted text-center">Software Engineer</p>
+              <p class="text-muted text-center">Abogado / Cliente</p>
 
             </div>
             <!-- /.box-body -->
@@ -87,37 +87,25 @@ if(!isset($_SESSION['usuario']))
           <!-- About Me Box -->
           <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">About Me</h3>
+              <h3 class="box-title">Informacion Personal</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
+              <strong><i class="fa fa-book margin-r-5"></i> Estudios:</strong>
 
               <p class="text-muted">
-                B.S. in Computer Science from the University of Tennessee at Knoxville
+                  La universidad de la calle
               </p>
 
               <hr>
 
-              <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
+              <strong><i class="fa fa-map-marker margin-r-5"></i> Ubicacion:</strong>
 
-              <p class="text-muted">Malibu, California</p>
-
-              <hr>
-
-              <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
-
-              <p>
-                <span class="label label-danger">UI Design</span>
-                <span class="label label-success">Coding</span>
-                <span class="label label-info">Javascript</span>
-                <span class="label label-warning">PHP</span>
-                <span class="label label-primary">Node.js</span>
-              </p>
+              <p class="text-muted">Sogamoso, Colombia</p>
 
               <hr>
 
-              <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+              <strong><i class="fa fa-file-text-o margin-r-5"></i> Notas</strong>
 
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
             </div>
@@ -129,9 +117,9 @@ if(!isset($_SESSION['usuario']))
         <div class="col-md-9">
           <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
-              <li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
-              <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
-              <li><a href="#settings" data-toggle="tab">Settings</a></li>
+              <li class="active"><a href="#activity" data-toggle="tab">Actividad</a></li>
+              <li><a href="#timeline" data-toggle="tab">Linea de Tiempo</a></li>
+              <li><a href="#settings" data-toggle="tab">Configuraciones</a></li>
             </ul>
             <div class="tab-content">
               <div class="active tab-pane" id="activity">
